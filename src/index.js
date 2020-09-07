e6b583fc6e511fb6c0e026d3145df8d56c25a7a4
glaubernespoli <glauber.nespoli@gmail.com>
2020-09-07 09:43:02 +0200

    chaning public path

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const path = require('path');
 const PORT = 5000;
 const server = express();
 const lrServer = livereload.createServer();
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../docs');
 
 server.use(connectLR());
 server.use(express.static(publicPath));
 lrServer.watch(publicPath);
 
 server.listen(PORT, () => {
-	console.log(`Server up on port ${PORT}.`);
+    console.log(`Server up on port ${PORT}.`);
 });
